6c5e8b46806ec056aba1e264760b08779b36990f
roshanclergo <157293187+round2fight@users.noreply.github.com>
2024-10-17 01:31:40 +0530

    Add Spinner component

---
+++ components/Spinner.js
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-t-5 border-transparent border-red-500"></div>
+    </div>
+  );
+};
+
+export default Spinner;
--- pages/uploader.js
+++ pages/uploader.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import axios from "axios";
 import Loader from "react-loader-spinner";
+import Spinner from "@/components/Spinner";
 const UploadImagePage = () => {
   const [file, setFile] = useState(null);
   const [extractedData, setExtractedData] = useState(null);
@@ -26,6 +27,10 @@ const UploadImagePage = () => {
       // setExtractedData(response.data);
     } catch (error) {
       console.error("Error uploading the file:" + error);
+      <Spinner />;
+      alert(
+        "Error processing image, Please wait for my free services to boot up."
+      );
     } finally {
       setLoading(false);
     }
@@ -71,89 +76,89 @@ const UploadImagePage = () => {
 
   return (
     <>
-      <div className="p-6 ">
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-          <div className="justify-center rounded-3xl overflow-hidden shadow-xl p-4 bg-nyanza max-w-md mx-auto">
-            <div className="m-4 flex justify-center items-center">
-              <div className="text-center">
-                <h1 className="text-gray-700 font-bold text-4xl">
-                  AirDeal Assignment
-                </h1>
-                <p className="text-xl text-gray-700">OCR using MERN stack</p>
-                <p className="text-l text-gray-700">By Roshan Daniel</p>
+      <div className="">
+        <div className="m-2">{loading === true && <Spinner />}</div>
+
+        <div className="justify-center rounded-3xl overflow-hidden shadow-xl p-4 bg-nyanza max-w-md mx-auto">
+          <div className="m-4 flex justify-center items-center">
+            <div className="text-center">
+              <h1 className="text-gray-700 font-bold text-4xl">
+                AirDeal Assignment
+              </h1>
+              <p className="text-xl text-gray-700">OCR using MERN stack</p>
+              <p className="text-l text-gray-700">By Roshan Daniel</p>
+            </div>
+          </div>{" "}
+          <div className="">
+            <div
+              {...getRootProps()}
+              style={{ border: "2px dashed #ccc", padding: "20px" }}
+            >
+              <input {...getInputProps()} />
+              <div className="text-center text-gray-700">
+                Drag & drop a visiting card image here, or click to select a
+                file
               </div>
-            </div>{" "}
-            <div className="">
-              <div
-                {...getRootProps()}
-                style={{ border: "2px dashed #ccc", padding: "20px" }}
-              >
-                <input {...getInputProps()} />
-                <div className="text-center text-gray-700">
-                  Drag & drop a visiting card image here, or click to select a
-                  file
+            </div>
+            <div className="flex justify-center">
+              {file ? (
+                <div className="mt-4">
+                  <h3 className="flex justify-center text-gray-700">
+                    Image Preview:
+                  </h3>
+                  <img
+                    className="flex justify-center"
+                    src={URL.createObjectURL(file)}
+                    alt="Preview"
+                    style={{ width: "400px" }}
+                  />
                 </div>
-              </div>
-              <div className="flex justify-center">
-                {file ? (
-                  <div className="mt-4">
-                    <h3 className="flex justify-center text-gray-700">
-                      Image Preview:
-                    </h3>
-                    <img
-                      className="flex justify-center"
-                      src={URL.createObjectURL(file)}
-                      alt="Preview"
-                      style={{ width: "400px" }}
-                    />
-                  </div>
-                ) : (
-                  <div className="mt-4 text-gray-700">
-                    Waiting for your upload :)
-                  </div>
-                )}
-              </div>
-              <div className="flex justify-center">
-                {" "}
-                <button
-                  className="m-3 disabled:bg-gray-400  disabled:hover:bg-gray-400  bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-300 "
-                  onClick={handleSubmit}
-                  disabled={!file}
-                >
-                  Process Image
-                </button>
-                <button
-                  className="m-3 disabled:bg-gray-400  disabled:hover:bg-gray-400  bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-300 "
-                  onClick={handleClear}
-                  disabled={!file}
-                >
-                  Clear Image
-                </button>
-              </div>
+              ) : (
+                <div className="mt-4 text-gray-700">
+                  Waiting for your upload :)
+                </div>
+              )}
+            </div>
+            <div className="flex justify-center">
+              {" "}
+              <button
+                className="m-3 disabled:bg-gray-400  disabled:hover:bg-gray-400  bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-300 "
+                onClick={handleSubmit}
+                disabled={!file}
+              >
+                Process Image
+              </button>
+              <button
+                className="m-3 disabled:bg-gray-400  disabled:hover:bg-gray-400  bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-300 "
+                onClick={handleClear}
+                disabled={!file}
+              >
+                Clear Image
+              </button>
             </div>
           </div>
-          <div className="justify-center rounded-3xl overflow-hidden shadow-xl p-4 bg-nyanza max-w-md mx-auto">
-            <div className="m-2 flex justify-center">
-              <button
-                className="m-3  bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-300 "
-                onClick={handleGetCards}
-              >
-                Get Cards
-              </button>
-              <button
-                className="m-3  bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-300 "
-                onClick={handleDeleteCards}
-              >
-                Delete All Cards
-              </button>
-            </div>
-            <div className="m-2 flex justify-center max-w-4xl">
-              {" "}
-              <CardPagination cards={cards} />;
-              {/* {cards.map((card) => (
+        </div>
+        <div className=" mt-2 justify-center rounded-3xl overflow-hidden shadow-xl p-4 bg-nyanza max-w-md mx-auto">
+          <div className="m-2 flex justify-center">
+            <button
+              className="m-3  bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-300 "
+              onClick={handleGetCards}
+            >
+              Get Cards
+            </button>
+            <button
+              className="m-3  bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-300 "
+              onClick={handleDeleteCards}
+            >
+              Delete All Cards
+            </button>
+          </div>
+          <div className="m-2 flex justify-center max-w-4xl">
+            {" "}
+            <CardPagination cards={cards} />;
+            {/* {cards.map((card) => (
                 <div key={card._id}>Yo</div>
               ))} */}
-            </div>
           </div>
         </div>
       </div>
